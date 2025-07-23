32cf7a081065acadf6ab32700972a8853925a62d
blueberry-adii <adithyap455@gmail.com>
2025-07-23 17:39:00 +0530

    Fixed Login controller logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,21 +13,25 @@ exports.LogIn = asyncHandler(async (req, res, next) => {
   if (!username || !password)
     throw new ApiError(400, "All Fields are required");
 
-  const user = User.findOne({ username }).select("+password");
+  const user = await User.findOne({ username }).select("+password");
 
   if (!user) throw new ApiError(400, "Invalid Credentials");
 
-  const isPasswordValid = bcrypt.compare(password, user.password);
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) throw new ApiError(400, "Invalid Credentials");
 
   return res.status(200).json(
-    new ApiResponse(201, {
-      _id: user._id,
-      name: user.name,
-      username: user.username,
-      role: user.role,
-    })
+    new ApiResponse(
+      201,
+      {
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        role: user.role,
+      },
+      "Login Successful"
+    )
   );
 });
 
